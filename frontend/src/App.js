d5745be1052e4209cddbe10b302edba1bacdbfd4
Antoine <antoinejuillard015@gmail.com>
2023-04-28 17:28:57 -0500

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,50 @@
+
+import './App.css';
+import data from "./data.js"
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    <a href='/'>Amazona</a>
+    <nav>
+      <ul>
+        <li>Products</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+      </header>
+
+    <main>
+
+      <h1>List products</h1>
+
+      
+      <div className='list-products'>
+        {
+          data.products.map(product => (
+          <div className='products' key={product.slug}>
+
+        <a to href='/'>
+        <img src={product.image} alt={product.name} />
+        </a>
+            <a href='/'>
+          <p>{product.name}</p>
+          </a>
+          <p><strong> {product.price}</strong></p>
+          <button>Add to cart</button>
+        </div>))
+        }
+        </div>
+      
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
