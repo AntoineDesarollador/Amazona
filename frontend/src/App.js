af6de4cf725ead432dfb4acd0630bb5f0ef9e9ac
Antoine <antoinejuillard015@gmail.com>
2023-04-29 04:27:50 -0500

    Mise en place du server.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,8 @@
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
-import data from "./data.js"
 import HomeScreen from "./screens/HomeScreen.js";
-
-
+import ProductScreen from './screens/ProductScreen';
 
 
 function App() {
@@ -12,35 +10,16 @@ function App() {
     <BrowserRouter>
     <div className="App">
       <header className="App-header">
-    <a href='/'>Amazona</a>
+    <Link to='/'>Amazona</Link>
 
       </header>
 
     <main>
     <Routes>
-    <Route   path='/' element={<HomeScreen />}/>
+    <Route path='/product/:slug' element={<ProductScreen />}/>
+    <Route path='/' element={<HomeScreen />}/>
     </Routes>
-      <h1>List products</h1>
-
-      
-      <div className='list-products'>
-        {
-          data.products.map(product => (
-          <div className='products' key={product.slug}>
-
-        <a href={`/product/${product.slug}`}>
-        <img src={product.image} alt={product.name} />
-        </a>
-            <a href={`/product/${product.slug}`} >
-          <p>{product.name}</p>
-          </a>
-          <p><strong>$ {product.price}</strong></p>
-          <button>Add to cart</button>
-        </div>))
-
-        }
-        </div>
-      
+ 
 
       </main>
     </div>
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,32 @@
 
+import { Link } from "react-router-dom";
+import data from "../data.js";
+
 function HomeScreen(){
     return(
-        <h1>Home Screen</h1>
+        <div>
+    <h1>List products</h1>
+
+      
+<div className='list-products'>
+  {
+    data.products.map(product => (
+    <div className='products' key={product.slug}>
+
+  <Link to={`/product/${product.slug}`}>
+  <img src={product.image} alt={product.name} />
+  </Link>
+      <Link to={`/product/${product.slug}`} >
+    <p>{product.name}</p>
+    </Link>
+    <p><strong>$ {product.price}</strong></p>
+    <button>Add to cart</button>
+  </div>))
+
+  }
+  </div>
+
+        </div>
     )
 }
 
