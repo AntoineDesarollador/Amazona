4f3c954ef129fc40131f3fe5a201a8a720c99dac
Antoine <antoinejuillard015@gmail.com>
2023-04-30 19:34:34 -0500

    Essaie site

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,23 @@
 import express from "express";
-
 import data from "./data.js";
 
+
+
+
+
 const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send(data.slug);
+});
+
 
 app.get("/api/product", (req, res) => {
     res.send(data.products);
 });
 
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`serve at http://localhost:${port}`);
 });
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,21 @@
 
 import { Link } from "react-router-dom";
 import data from "../data.js";
+import { useEffect, useState } from "react";
 
 function HomeScreen(){
+
+const [products, setProducts] = useState([]);
+useEffect(() => {
+  const fetchData = async() => {
+    const result = await axios.get("http://localhost:3000/api/products");
+    setProducts(result.data);
+  }; 
+  fetchData();
+}, []);
+
     return(
-        <div>
+<div>
     <h1>List products</h1>
 
       
@@ -14,7 +25,7 @@ function HomeScreen(){
     <div className='products' key={product.slug}>
 
   <Link to={`/product/${product.slug}`}>
-  <img src={product.image} alt={product.name} />
+  <img src={product.image} alt={product.name} value={product.slug} />
   </Link>
       <Link to={`/product/${product.slug}`} >
     <p>{product.name}</p>
@@ -25,8 +36,7 @@ function HomeScreen(){
 
   }
   </div>
-
-        </div>
+</div>
     )
 }
 
